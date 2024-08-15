36a9eea66f069ae1eb719ad15d44de2e56f16e80
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2024-08-15 20:14:25 +0100

    first commit

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quotes2/app_bindings/app_bindings.dart';
+import 'package:quotes2/routes/routes.dart';
 
 
 class MyApp extends StatelessWidget {
